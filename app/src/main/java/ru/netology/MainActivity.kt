1aa8dde4c88b378065d4bce8c9b0fe5eecf7207c
nicys <nicys2476@yandex.ru>
2021-03-18 22:54:26 +0700

    test

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import ru.netology.adapter.OnInteractionListener
 import ru.netology.adapter.PostsAdapter
 import ru.netology.databinding.ActivityMainBinding
 import ru.netology.util.AndroidUtils
@@ -17,14 +18,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter(
-                onLikeListener = { viewModel.likeById(it.id) },
-                onShareListener = { viewModel.shareById(it.id) },
-                onRemoveListener = { viewModel.removeById(it.id) }
-        )
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            }
+        })
         binding.list.adapter = adapter
         viewModel.data.observe(this, { posts ->
             adapter.submitList(posts)
+        })
+
+        viewModel.edited.observe(this, { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.contentPost) {
+                requestFocus()
+                setText(post.content)
+            }
         })
 
         binding.save.setOnClickListener {
